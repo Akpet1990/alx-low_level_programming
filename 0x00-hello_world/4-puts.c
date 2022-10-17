0eb1cb49a3c52a8d05e1f83ddaa6c0c58e35858e
Akpet1990 <peter.babatunde91@gmail.com>
2022-10-17 13:30:22 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ *Description: "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char Peter[] = "\"Programming is like building a multilingual puzzle";
+
+  puts(Peter);
+  
+  return (0);
+  
+}
